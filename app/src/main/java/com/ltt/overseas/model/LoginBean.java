f9c60b39af549a5a63466f938275770718238ccc
xj <jxu@iyunwen.com>
2018-05-14 15:14:44 +0800

    xj_add chat list and chat page

---
+++ app/src/main/java/com/ltt/overseas/model/LoginBean.java
@@ -0,0 +1,26 @@
+package com.ltt.overseas.model;
+
+/**
+ * Created by yunwen on 2018/5/14.
+ */
+
+public class LoginBean {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String email;
+    private String password;
+}
